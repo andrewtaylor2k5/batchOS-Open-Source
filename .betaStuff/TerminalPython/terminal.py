67314743c987c84449aeb4a9e76eeacea226e4af
Andrew Taylor <44929427+andrewtaylor2k5@users.noreply.github.com>
2018-11-10 19:16:30 +0000

    batchOS Beta Component || Python Terminal

---
+++ .betaStuff/TerminalPython/terminal.py
@@ -0,0 +1,30 @@
+from random import *
+print()
+print("Python Terminal")
+print()
+print("Type $help for help information")
+def terminal():
+  command=input("Enter Command >>>  ")
+  if command== ("$help"):
+    print()
+    print("Python Terminal Help Information")
+    print()
+    print("$help")
+    print("Displays this help information")
+    print()
+    print("$random")
+    print("Displays a random number")
+    print()
+    print("$exit")
+    print("Exits the Terminal")
+    print()
+    terminal()
+  elif command== ("$random"):
+    randomnumber= randint(1,999999999999999999999999999999)
+    print()
+    print("Randomly Generated Number:")
+    print(randomnumber)
+    print()
+    terminal()
+  
+terminal()
